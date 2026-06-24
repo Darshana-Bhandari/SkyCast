761867f6f481d0cf676eac04041214a89196f53c
Darshana-Bhandari <bandaridarshana@gmail.com>
2026-06-24 15:01:44 +0545

    Update weather app

--- javascript.js
+++ javascript.js
@@ -1,20 +1,23 @@
-const apikey="";
-const apiurl="https://api.openweathermap.org/data/2.5/weather?q=germany&appid=01224752f6d7948fe5d5c12b31fee4e4&units=metric&q=bangalore";
+const apikey = "01224752f6d7948fe5d5c12b31fee4e4";
+const apiurl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
+const searchbox = document.querySelector(".search input");
+const searchbtn = document.querySelector(".search button");
 
-const searchbox=document.querySelector(".search input");
-consrt searchbtn=document.querySelector(".search button");
-async function checkWeather(city){
-    const response=await fetch(apiurl + city + '&appid=${apikey}');
-    var data=await response.json();
+async function checkWeather(city) {
+    const response = await fetch(apiurl + city + '&appid=${apikey}');
+    const data = await response.json();
 
     console.log(data);
 
-    document.querySelector(".city").innerHTML=data.name;
-    document.querySelector(".temp").innerHTML= Math.round(data.main.temp) + "°C";
-    document.querySelector(".humidity").innerHTML=data.main.humidity + "%";
-    document.querySelector(".wind").innerHTML=data.wind.speed + " km/h";
-
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 }
+
+searchbtn.addEventListener("click", () => {
+    checkWeather(searchbox.value);
+});
 
 checkWeather();
